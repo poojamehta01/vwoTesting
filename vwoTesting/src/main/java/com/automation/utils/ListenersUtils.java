37a9150654aec1898da01de8bb457bf8189f567d
pooja mehta <43508863+poojamehta01@users.noreply.github.com>
2021-04-22 09:53:11 +0530

    Update ListenersUtils.java
    added Comments

--- vwoTesting/src/main/java/com/automation/utils/ListenersUtils.java
+++ vwoTesting/src/main/java/com/automation/utils/ListenersUtils.java
@@ -1,3 +1,12 @@
+/****
+ * OOPS CONCEPT - Method OVER RIDING
+ * here ListenersUtils - subclass has same methods as ITestListener - super class
+ * subclass over rides method of parent class without touching source code of parent class ... 
+ * parent class does not have any control over the implemenation of subclass
+ * applicable only to inherited methods, 
+ * overriding method can have diff. return types, cannot have restrictive access modifier
+ * final and static methods and constructor cant be overridden 
+ */
 package com.automation.utils;
 
 import org.testng.ITestListener;
