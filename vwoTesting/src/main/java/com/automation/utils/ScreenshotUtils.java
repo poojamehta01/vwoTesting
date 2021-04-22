4f987c4e9eadbd52089323f31781084cbbaeea6f
pooja mehta <43508863+poojamehta01@users.noreply.github.com>
2021-04-22 09:52:32 +0530

    Update ScreenshotUtils.java
    added Comments

--- vwoTesting/src/main/java/com/automation/utils/ScreenshotUtils.java
+++ vwoTesting/src/main/java/com/automation/utils/ScreenshotUtils.java
@@ -1,3 +1,13 @@
+/*****
+ * OOP CONCEPT USED - INHERITANCE - 
+ * driverutils is used to initialize webDriver interface -PARENT CLASS
+ * and now it is extended in screenshot utils  - CHILD CLASS
+ * child class inherits all the properties - data members & methods of parent class,
+ *  plus we can also create new methods and data members in Child class
+ *  if properties in parent class are private then those prop cannot be inherited therefore webdriver is static 
+ ABSTRACTION - HIDE implementation of internal details and just show functionality 
+ 
+ */
 package com.automation.utils;
 import static com.automation.utils.LogUtils.Log;
 import static com.automation.utils.WebElementUtils.*;
