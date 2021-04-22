8965d58b55332f4a1351c7e16727a4ad53413498
pooja mehta <43508863+poojamehta01@users.noreply.github.com>
2021-04-22 09:50:31 +0530

    Update HomePageSteps.java

--- vwoTesting/src/main/java/com/automation/steps/HomePageSteps.java
+++ vwoTesting/src/main/java/com/automation/steps/HomePageSteps.java
@@ -1,3 +1,27 @@
+/****
+ * selenium webDriver architecture 
+ * selenium binding language - JAVA
+ * JSON WIRE PROTOCOL
+ * HTTP OVER HTTP SERVER - Browser drivers, real Browsers
+ * 
+ * 
+ * screenShot capturing implement in ScreenshotsUtils
+ * execution of JavaScript functions implemented in CommonUtils
+ * delete browser cookes implemented in DriverUtils
+ * capture pageTitle implemented in AssertionUtils
+ * ASSERT VS VERIFY - assert if fails execution stops immediatiely but in verify it does not
+ * SOFT VS HARD ASSERT - SOFT - error collected and execution continues but in HARD it stops immediately
+ * 
+ * VERIFCATION POINTS IMPLEMENTED - Check page title
+ * check certain text
+ * check certain element - button/dropdown
+ * check CLick operation
+ * check submit operation
+ * check navigation from one link to another
+ * 
+ * WAIT implemented in WebElementUtils - webDriverWAit
+ */
+
 package com.automation.steps;
 
 import org.testng.annotations.Listeners;
